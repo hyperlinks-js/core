8f705e0feb4b4dabba998bd5ddf89186a34b9368
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2022-05-19 07:31:23 +0530

    fix: remove loggers

--- src/index.js
+++ src/index.js
@@ -6,26 +6,20 @@ import { extractMetaTags } from './lib/_extract.js'
 import { tooltipInit } from '@hyperlinks-js/tooltip'
 
 export const Init = async (tipOption) => {
-  if (Checker('localStorage')) {
-    console.log('localStorage is supported')
-  } else {
+  if (!Checker('localStorage')) {
     throw new Error('localStorage is not supported')
   }
   const NATIVE = [...document.getElementsByClassName('hyperlinks')].map((x) => x.href)
   if (!NATIVE.length) {
     console.warn('Running with No Hyperlinks. \n\n If you think this is wrong please refer the docs properly : https://github.com/Criticalcarpet/Hyperlinks#readme')
-    return false
+    return
   }
   const FEEDER = [...new Set(NATIVE)]
-  console.log(FEEDER)
   const NEW = HasMeta(FEEDER)
-  console.log(NEW)
   if (NEW.length > 0) {
     const DATA = await GetHtml(FEEDER)
-    console.log(DATA)
     for (let i = 0; i < DATA.length; i++) {
       const DOM = ParseDOM(DATA[i].contents)
-      console.log(DOM)
       const NEEDEDMETA = ['og:image', 'og:url', 'og:title', 'og:description']
       extractMetaTags(DOM, NEEDEDMETA, DATA[i].status.url)
     }
